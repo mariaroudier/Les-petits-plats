76dc40a9dbdf1d975095acfc2cd86d64a9a8114a
mariaroudier <volokh.maria@gmail.com>
2022-08-11 11:00:04 +0200

    cree la boite avec les elements choisi, par click sur span cet element est ajoute dans la boite avec le croix

--- main.js
+++ main.js
@@ -2,11 +2,29 @@ import { recipes } from "/data/recipes.js"
 import Recipe from "./scripts/Recipe.js";
 import Search from "./scripts/Search.js";
 
-/* DOM */
+// DOM 
 const recipesGrid = document.getElementById('recipes-grid')
 let allRecipes = []
 let search;
-// отобразить рецепты при загрузке страницы
+
+      // ingredients
+const boxIngredients = document.getElementById('box-input-ingredient')
+const inputIngredients = document.getElementById('search-ingredient')
+const chevronIngredient = document.getElementById('chevron-ingredient')
+const showedIngredients = document.getElementById('all-ingredients')
+      // appareiles
+const boxAppareils = document.getElementById('box-input-appareiles')
+const inputAppareils = document.getElementById('search-appareiles')
+const chevronAppareils = document.getElementById('chevron-appareiles')
+const showedAppareils = document.getElementById('all-appareils')
+      // dishes
+const boxDishes = document.getElementById('box-input-dishes')
+const inputDishes = document.getElementById('search-dishes')
+const chevronDishes = document.getElementById('chevron-dishes')
+const showedDishes = document.getElementById('all-dishes')
+
+
+// montrer toute les recettes sur la page
 function displayData(recipes) {
       recipes.forEach((recipe) => {
             const recipeModel = new Recipe(recipe);
@@ -20,39 +38,91 @@ function init() {
       displayData(recipes);
       search  = new Search(allRecipes);
 }
-// input search
+
 document.getElementById('search-input').addEventListener('input', (e) => {
-
       search.toSearchRecipe(e.target.value)
 })
 
-// input ingredients
-document.getElementById('search-ingredient').addEventListener('input', (e) => {
-      search.toSearchRecipe(e.target.value)
-      
+//chercher ingredients
+function toShowIngredients() {
+      if(!chevronIngredient.classList.contains('fa-chevron-up')) {
+            showedIngredients.style.display = 'flex'
+            boxIngredients.style.width = '100%'
+            inputIngredients.setAttribute("placeholder", "Rechercher un ingredient")
+      } else if(chevronIngredient.classList.contains('fa-chevron-up')) {
+            showedIngredients.style.display = 'none'
+            boxIngredients.style.width = '150px'
+            inputIngredients.setAttribute("placeholder", "Ingredients")
+      }
+}
+
+chevronIngredient.addEventListener('click', (e) => {
+      toShowIngredients(e)
+      chevronIngredient.classList.toggle('fa-chevron-up')
 })
 
-// // input appareils
-// document.getElementById('search-appareiles').addEventListener('input', (e) => {
-//       search.toSearchAppareil(e.target.value)
-// })
+if(chevronIngredient.classList.contains('fa-chevron-up')) {
+      inputIngredients.addEventListener('focus', (e) => {
+            toShowIngredients(e)
+      })
+}
 
-// // input ustensiles
-// document.getElementById('search-diches').addEventListener('input', (e) => {
-//       search.toSearchUstensil(e.target.value)
-// })
+// chercher appareiles
+function toShowAppareils() {
+      if(!chevronAppareils.classList.contains('fa-chevron-up')) {
+            document.getElementById('all-appareils').style.display = 'flex'
+            boxAppareils.style.width = '100%'
+            inputAppareils.setAttribute("placeholder", "Rechercher un appareil")
+            
+      } else if(chevronAppareils.classList.contains('fa-chevron-up')) {
+            document.getElementById('all-appareils').style.display = 'none'
+            boxAppareils.style.width = '150px'
+            inputAppareils.setAttribute("placeholder", "Appareils")
+      }
+}
 
+chevronAppareils.addEventListener('click', (e) => {
+      toShowAppareils(e)
+      chevronAppareils.classList.toggle('fa-chevron-up')
+})
+if(chevronAppareils.classList.contains('fa-chevron-up')) {
+      inputAppareils.addEventListener('focus', (e) => {
+            toShowAppareils(e)
+      })
+}
 
+// chercher dishes
+function toShowDishes() {
+      if(!chevronDishes.classList.contains('fa-chevron-up')) {
+            document.getElementById('all-dishes').style.display = 'flex'
+            boxDishes.style.width = '100%'
+            inputDishes.setAttribute("placeholder", "Rechercher un ustensile")
+      } else if(chevronDishes.classList.contains('fa-chevron-up')) {
+            document.getElementById('all-dishes').style.display = 'none'
+            boxDishes.style.width = '150px'
+            inputDishes.setAttribute("placeholder", "Ustensiles")
+      }
+}
 
-// document.getElementById('chevron-ingredient').addEventListener('click', (e) => {
-//       search.toSearchRecipe(e.target.value)
-// })
+chevronDishes.addEventListener('click', (e) => {
+      toShowDishes(e)
+      chevronDishes.classList.toggle('fa-chevron-up')
+})
+if(chevronDishes.classList.contains('fa-chevron-up')) {
+      inputDishes.addEventListener('focus', (e) => {
+            toShowDishes(e)
+      })
+}
 
-// form.addEventListener('submit',function(ev) {
-//       // on arrete l'envoy de forme par default
-//       ev.preventDefault() 
+// choisir un tag
 
+// document.querySelector(".span").addEventListener('click', (e) => {
+//       console.log('!!!')
+//       toShowDishes(e)
+//       // let chosenSpan = document.createElement('div')
+//       // chosenSpan.appendChild()
 // })
 
 init(); 
+
 
--- scripts/Search.js
+++ scripts/Search.js
@@ -6,23 +6,22 @@ export default class Search {
       }
 
       toSearchRecipe(inputValue) {
-            this.input = inputValue // не удалять
+            this.input = inputValue
             let recipesArray = []
-            let ingredientsInRecipe = [] // all ingredients of all recipes
-            let matchedAppareils = [] // подходящие аппареи
-            let matchedUstensils = [] //  подходящая посуда
+            let matchedAppareils = []
+            let matchedUstensils = []
             let matchedIngredients = []
+            let spans = []
             
             document.getElementById('recipes-grid').innerHTML = ''
-            
             // matching le recipe avec input (titre, description, ingredients)
             if(this.input.length >= 3) {
                   this.recipes.forEach(recipe => {
+                        let ingredientsInRecipe = []
                         recipe.ingredients.forEach(elem => {
                               ingredientsInRecipe.push(elem.ingredient)
                         })
-                        
-                        if(recipe.name.toLowerCase().match(inputValue) || recipe.description.toLowerCase().match(inputValue) || ingredientsInRecipe.toString().toLowerCase().match(inputValue)) {
+                        if(recipe.name.toLowerCase().includes(inputValue) || recipe.description.toLowerCase().includes(inputValue) || ingredientsInRecipe.toString().toLowerCase().includes(inputValue)) {
                               recipesArray.push(recipe)
                               matchedAppareils.push(recipe.appliance)
                               matchedUstensils.push(recipe.ustensils)
@@ -35,6 +34,14 @@ export default class Search {
                   
             } else { // si input.length < 3
                   recipesArray = this.recipes
+                  recipesArray.forEach(recipe => {
+                        matchedAppareils.push(recipe.appliance)
+                        matchedUstensils.push(recipe.ustensils)
+                        recipe.ingredients.forEach(elem => {
+                              matchedIngredients.push(elem.ingredient)
+                        })
+                  })
+                  
             }
            
 
@@ -48,11 +55,13 @@ export default class Search {
             const setIngredients = new Set(matchedIngredients)
             document.getElementById('all-ingredients').innerHTML = ''
             setIngredients.forEach(elem => {
-                        let spanIngredient = document.createElement('span')
-                        spanIngredient.textContent = elem
-                        document.getElementById('all-ingredients').appendChild(spanIngredient)
+                  let spanIngredient = document.createElement('span')
+                  spanIngredient.textContent = elem
+                  spanIngredient.classList = 'span'
+                  spans.push(spanIngredient)
+                  document.getElementById('all-ingredients').appendChild(spanIngredient)
                         
-                  })
+            })
             
             // Set appareiles
             const setAppareils = new Set(matchedAppareils)
@@ -60,6 +69,8 @@ export default class Search {
             setAppareils.forEach(appareil => {
                   let spanAppareil = document.createElement('span')
                   spanAppareil.textContent = appareil
+                  spanAppareil.classList = 'span'
+                  spans.push(spanAppareil)
                   document.getElementById('all-appareils').appendChild(spanAppareil)
             })
 
@@ -72,13 +83,29 @@ export default class Search {
             })
 
             const setUstensils = new Set(subUtensil)
-            document.getElementById('all-diches').innerHTML = ''
+            document.getElementById('all-dishes').innerHTML = ''
             setUstensils.forEach(ustensil => {
                   let spanUstensil = document.createElement('span')
                   spanUstensil.textContent = ustensil
-                  document.getElementById('all-diches').appendChild(spanUstensil)  
+                  spanUstensil.classList = 'span'
+                  spans.push(spanUstensil)
+                  document.getElementById('all-dishes').appendChild(spanUstensil)  
             })
             
+            // ajouter choisi span dans le searche
+            spans.forEach(elem => {
+                  elem.addEventListener('click', () => {
+                        const boxForChosen = document.createElement('div')
+                              boxForChosen.classList = "box-for-chosen"
+                        const croix = document.createElement('span')
+                              croix.classList = "fa-regular fa-circle-xmark"
+                        document.getElementById('chosen').appendChild(boxForChosen)
+                        boxForChosen.appendChild(elem)
+                        boxForChosen.appendChild(croix)
+                        
+                  })
+            })
+
       }
 
 }
